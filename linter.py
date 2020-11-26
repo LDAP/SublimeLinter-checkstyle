9b4bcc9ef97666edd183025c86f9ec59b1946ee2
Lucas Alber <lucasd.alber@gmail.com>
2020-11-26 23:19:51 +0100

    Delete unused constant

--- linter.py
+++ linter.py
@@ -92,7 +92,6 @@ DOWNLOAD_BASE_URL = 'https://github.com/checkstyle/'\
 VERSIONS_XML_URL = 'https://repo1.maven.org/maven2/'\
                    'com/puppycrawl/tools/checkstyle/'\
                    'maven-metadata.xml'
-DEBUG_PANEL_NAME = 'SublimeLinter-checkstyle'
 CACHE_FOLDER_NAME = 'SublimeLinter-checkstyle'
 
 
